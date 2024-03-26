a39a446b8c81125d9c6625f4e77a2ec55c01d6e9
Kelvin M <kelkm609@gmail.com>
2024-03-26 09:19:58 -0400

    Initial commit, updated app and tested endpoint

--- index.test.js
+++ index.test.js
@@ -3,6 +3,7 @@ const { execSync } = require('child_process');
 execSync('npm install');
 execSync('npm run seed');
 
+
 const request = require("supertest")
 const { db } = require('./db/connection');
 const { Musician } = require('./models/index')
@@ -12,11 +13,16 @@ const seedMusician = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test("Testing musicians endpoint", async () => {
+       
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(response.statusCode).toBe(200);
+    })
     
-    
 
 
 
 
     
-})
+});
