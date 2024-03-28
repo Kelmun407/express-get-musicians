0c6a83d250423f75ce61e8bdd2a3e8cce3f3fadd
KelvinM <kelkm609@gmail.com>
2024-03-28 15:29:13 -0400

    added express router

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,35 @@
 const express = require("express");
 const musicianRouter = express.Router();
+const { Musician } = require("../models/index");
+
+
+musicianRouter.get("/", async (req, res) =>{
+    const musicians = await Musician.findAll({});
+    res.json(musicians);
+});
+
+musicianRouter.get("/:id", async (req, res) =>{
+    const artistId = await Musician.findByPk(req.params.id);
+    res.json(artistId);
+});
+
+musicianRouter.post("/", async (req, res) => {
+    const newArtist = await Musician.create(req.body);
+    res.json(newArtist);
+});
+
+
+musicianRouter.put("/:id", async(req, res) => {
+    const user = await Musician.findByPk(req.params.id);
+    const updatedUser = await user.update(req.body);
+    res.json(updatedUser);
+});
+
+musicianRouter.delete("/:id", async(req, res) =>{
+   const user = await Musician.findByPk(req.params.id);
+   const delArtist = await user.destroy(req.body)
+   res.json(delArtist);
+});
 
 
 
--- src/app.js
+++ src/app.js
@@ -3,39 +3,15 @@ const app = express();
 const { Musician } = require("../models/index")
 const { db } = require("../db/connection")
 
+const musicianRouter = require("../routes/routes")
+app.use('/musicians', musicianRouter)
+
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 app.use(express.json());
 app.use(express.urlencoded())
 
-app.get("/musicians", async (req, res) =>{
-    const musicians = await Musician.findAll({});
-    res.json(musicians);
-});
-
-app.get("/musicians/:id", async (req, res) =>{
-    const artistId = await Musician.findByPk(req.params.id);
-    res.json(artistId);
-});
-
-app.post("/musicians", async (req, res) => {
-    const newArtist = await Musician.create(req.body);
-    res.json(newArtist);
-});
-
-
-app.put("/musicians/:id", async(req, res) => {
-    const user = await Musician.findByPk(req.params.id);
-    const updatedUser = await user.update(req.body);
-    res.json(updatedUser);
-});
-
-app.delete("/musicians/:id", async(req, res) =>{
-   const user = await Musician.findByPk(req.params.id);
-   const delArtist = await user.destroy(req.body)
-   res.json(delArtist);
-});
 
 
 
